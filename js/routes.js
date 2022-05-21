6694b602ee8e9aa5c99cce8c0ea81b08f10f2bcd
Vika <vika1super2014@gmail.com>
2022-05-21 15:28:18 +0300

    First commit

---
+++ js/routes.js
@@ -0,0 +1,14 @@
+const routes = [
+  {
+    path: '/',
+    template: '<h1></h1>'
+  },
+  {
+    path: '/play',
+    template: '<h1></h1>',
+  },
+  {
+    path: '/rules',
+    template: '<h1></h1>',
+  },
+];
